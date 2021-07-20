39748c4d6a3fd1600b7e8fcb2b16f084d5191fe7
James Phillips <phillips.james@gmail.com>
2021-07-20 16:20:46 -0500

    not sure why I was focused on isolated subnet routes here

--- routes.tf
+++ routes.tf
@@ -1,7 +1,7 @@
-resource "aws_route" "isolated" {
+resource "aws_route" "private" {
   for_each = var.transit_gateway_routes
 
-  route_table_id         = aws_route_table.isolated[0].id
+  route_table_id         = aws_route_table.private[0].id
   destination_cidr_block = each.value["destination_cidr_block"]
   transit_gateway_id     = each.value["transit_gateway_id"]
 
