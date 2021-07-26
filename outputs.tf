8cae55d54b6a56167d3bc73b64936588b669589c
James Phillips <phillips.james@gmail.com>
2021-07-26 14:32:23 -0500

    adding vpc_cidr_block output from parent module

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = concat(aws_vpc.this.*.id, [""])[0]
 }
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = concat(aws_vpc.this.*.cidr_block, [""])[0]
+}
 output "private_subnets" {
   description = "List of IDs of private subnets"
   value       = aws_subnet.private.*.id
